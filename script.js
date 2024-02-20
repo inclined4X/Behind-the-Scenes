002ff6d3b2492349a034062e1448d7ef6ea622ad
inclined4X <jezemiahsam48@gmail.com>
2024-02-20 17:53:15 +0000

    print age

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
   return age;
 }
-
 const firstName = 'Jonas';
 calcAge(1991);
