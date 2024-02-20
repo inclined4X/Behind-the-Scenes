fe160b4333eedc915942cce64d48927f68b632a1
inclined4X <jezemiahsam48@gmail.com>
2024-02-20 18:07:13 +0000

    millenial

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName} you are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
   }
   printAge();
   return age;
