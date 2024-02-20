935d8aca70a4b30380fec19f757bae9da02a3cfa
inclined4X <jezemiahsam48@gmail.com>
2024-02-20 22:51:19 +0000

    hoisting

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-'use strict';
+// 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName} you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName} you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // creating NEW variable with the same name as outer scope variables
-      const firstName = 'Steven';
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // creating NEW variable with the same name as outer scope variables
+//       const firstName = 'Steven';
 
-      // reassinging outer scope's variable
-      output = 'NEW OUTPUT';
-      const str = `oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       // reassinging outer scope's variable
+//       output = 'NEW OUTPUT';
+//       const str = `oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
 
-    // console.log(str);
-    console.log(millenial);
-    //   console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
-  return age;
-}
-const firstName = 'Jonas';
-calcAge(1991);
-// console.log(age);
+//     // console.log(str);
+//     console.log(millenial);
+//     //   console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // console.log(age);
+
+//HOISTING
+
+var me = 'Jons';
+let job = 'teacher';
+const year = 1991;
