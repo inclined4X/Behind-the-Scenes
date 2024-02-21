d50dd61b260106eb19a815c3384d38799d0f4b31
inclined4X <jezemiahsam48@gmail.com>
2024-02-21 19:26:46 +0000

    arrow functions do not have argument object but regular and expressive functions do

--- script.js
+++ script.js
@@ -156,3 +156,17 @@ const jonas = {
 jonas.greet();
 jonas.calcAge();
 // function inside a method
+
+// Arugments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
