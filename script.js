256f12505991c1724077e0fd1d6a28c13a61004e
inclined4X <jezemiahsam48@gmail.com>
2024-02-21 00:34:58 +0000

    method borrowing

--- script.js
+++ script.js
@@ -79,18 +79,18 @@
 // console.log(z === window.z);
 // console.log(this);
 
-console.log(this);
+// console.log(this);
 
 const calcAge = function (birthYear) {
   console.log(2037 - birthYear);
-  console.log(this);
+  // console.log(this);
 };
 
 calcAge(1991);
 
 const calcAgeArrow = birthYear => {
   console.log(2037 - birthYear);
-  console.log(this);
+  // console.log(this);
 };
 
 calcAgeArrow(1980);
@@ -104,3 +104,12 @@ const jonas = {
 };
 
 jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+// method borrowing
+matilda.calcAge = jonas.calcAge;
+// console.log(matilda);
+matilda.calcAge();
