49fe2a9b801ed683c59df694e729f1b462de4dcb
inclined4X <jezemiahsam48@gmail.com>
2024-02-21 19:03:30 +0000

    using self or that for this, in functions in methods

--- script.js
+++ script.js
@@ -127,9 +127,11 @@ const jonas = {
     // console.log(this);
     console.log(2037 - this.year);
 
+    const self = this; // self or that
     const isMillenial = function () {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996);
+      // console.log(this.year >= 1981 && this.year <= 1996);
     };
     isMillenial();
   },
